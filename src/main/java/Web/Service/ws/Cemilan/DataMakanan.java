83c5ac7051e39ff8bca9df1eae6889a7be0d923b
Harry Nur <harry.nur.ft19@mail.umy.ac.id>
2022-01-06 14:45:45 +0700

    Data 2 Apache

--- src/main/java/Web/Service/ws/Cemilan/DataMakanan.java
+++ src/main/java/Web/Service/ws/Cemilan/DataMakanan.java
@@ -6,28 +6,55 @@ package Web.Service.ws.Cemilan;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author harry
  */
-@Controller
+@CrossOrigin
+@RestController
+@RequestMapping("/data")
 public class DataMakanan {
-    @RequestMapping("/Eeting")
-    @ResponseBody
-    public List<Makanan> getMakanan(){
-        List<Makanan> DataMakanan = new ArrayList<>();
-        
-        MakananJpaController controller = new MakananJpaController() ;
-        
-        try {
-            DataMakanan = controller.findMakananEntities();
-        }catch (Exception e){}
-        
-        return DataMakanan;
+    @Autowired
+    private DataMakanan eat;
+    
+    @GetMapping("/Eating")
+    public List<DataMakanan> getAllMakanan(){
+        return eat.getAllMakanan();
     }
     
+    @GetMapping ("/Eating/{IDMakan}")
+    public DataMakanan getMakanById(@PathVariable String IDMakan){
+        return eat.getMakanById(IDMakan);
+    }
+    
+    @PostMapping ("/Eating")
+    public DataMakanan saveDataMakananDetails(@RequestBody DataMakanan s){
+        return eat.saveDataMakananDetails(s);
+    }
+    
+    @PutMapping("/Eating")
+    public DataMakanan updateDataMakanan(@RequestBody DataMakanan s){
+        return eat.updateDataMakanan(s);
+    }
+    
+    @DeleteMapping("/Eating/{IDMakan}")
+    public ResponseEntity<HttpStatus> deleteDataMakananById(@PathVariable String IDMakan){
+        eat.deleteDataMakananById(IDMakan);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
